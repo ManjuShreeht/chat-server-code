1a7b9d96df381dfc24fb74eea4517d7490303691
manju <manju@gmail.com>
2023-05-01 22:59:58 +0530

    commit

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,22 @@
 const express=require("express");
 const { Register, Login, Logout } = require("../controllers/authController");
+const User = require("../models/userModel");
 
 const router=express.Router();
 
 router.post('/register',Register)
 router.post('/login',Login)
 router.post('/logout',Logout)
+router.get('/hi',async(req,res)=>{
+    try {
+        console.log('hi')
+        const user=await User.find({})
+        return res.send(user)
+    } catch (error) {
+        console.log(error)
+    }
+
+})
 
 
 
